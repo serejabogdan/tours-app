fdbe1035e40cbd2b4069bae389861712df6aeafa
Sergey <serejabgdn@gmail.com>
2021-04-22 19:53:42 +0300

    created: TourServices

---
+++ src/components/Tour/TourServices/TourServices.js
@@ -0,0 +1,218 @@
+import React from 'react';
+import './TourServices.css';
+
+function TourServices() {
+  return (
+    <div class="services">
+      <h2 class="title-h2">Послуги</h2>
+
+      <div class="services__nav">
+        <ul class="services__list list">
+          <li>
+            Для дітей
+            <div class="sub-list-wrapper">
+              <ul class="sub__list list">
+                <li>
+                  <img class="check" src="./assets/check.svg" alt="it is an icon CHECK" />
+                  Водні горки: 3
+                </li>
+                <li>
+                  <img class="check" src="./assets/check.svg" alt="it is an icon CHECK" />
+                  дитяча дискотека
+                </li>
+                <li>
+                  <img class="check" src="./assets/check.svg" alt="it is an icon CHECK" />
+                  відділ в дитячому басейні для дорослих (2 без підігріву)
+                </li>
+              </ul>
+            </div>
+          </li>
+          <li>
+            Пляж
+            <div class="sub-list-wrapper">
+              <ul class="sub__list list">
+                <li>
+                  <img class="check" src="./assets/check.svg" alt="it is an icon CHECK" />
+                  бар на пляжі: платний
+                </li>
+                <li>
+                  <img class="check" src="./assets/check.svg" alt="it is an icon CHECK" />
+                  на пляже полотенца (из отеля): бесплатно
+                </li>
+                <li>
+                  <img class="check" src="./assets/check.svg" alt="it is an icon CHECK" />
+                  рекомендуется специальная обувь
+                </li>
+                <li>
+                  <img class="check" src="./assets/check.svg" alt="it is an icon CHECK" />
+                  автобус до пляжа: бесплатно
+                </li>
+                <li>
+                  <img class="check" src="./assets/check.svg" alt="it is an icon CHECK" />
+                  коралловый (Palma Beach)
+                </li>
+                <li>
+                  <img class="check" src="./assets/check.svg" alt="it is an icon CHECK" />
+                  бар на пляжі: платний
+                </li>
+                <li>
+                  <img class="check" src="./assets/check.svg" alt="it is an icon CHECK" />
+                  на пляже полотенца (из отеля): бесплатно
+                </li>
+                <li>
+                  <img class="check" src="./assets/check.svg" alt="it is an icon CHECK" />
+                  рекомендуется специальная обувь
+                </li>
+                <li>
+                  <img class="check" src="./assets/check.svg" alt="it is an icon CHECK" />
+                  автобус до пляжа: бесплатно
+                </li>
+                <li>
+                  <img class="check" src="./assets/check.svg" alt="it is an icon CHECK" />
+                  коралловый (Palma Beach)
+                </li>
+                <li>
+                  <img class="check" src="./assets/check.svg" alt="it is an icon CHECK" />
+                  бар на пляжі: платний
+                </li>
+                <li>
+                  <img class="check" src="./assets/check.svg" alt="it is an icon CHECK" />
+                  на пляже полотенца (из отеля): бесплатно
+                </li>
+                <li>
+                  <img class="check" src="./assets/check.svg" alt="it is an icon CHECK" />
+                  рекомендуется специальная обувь
+                </li>
+                <li>
+                  <img class="check" src="./assets/check.svg" alt="it is an icon CHECK" />
+                  автобус до пляжа: бесплатно
+                </li>
+                <li>
+                  <img class="check" src="./assets/check.svg" alt="it is an icon CHECK" />
+                  коралловый (Palma Beach)
+                </li>
+                <li>
+                  <img class="check" src="./assets/check.svg" alt="it is an icon CHECK" />
+                  бар на пляжі: платний
+                </li>
+                <li>
+                  <img class="check" src="./assets/check.svg" alt="it is an icon CHECK" />
+                  на пляже полотенца (из отеля): бесплатно
+                </li>
+                <li>
+                  <img class="check" src="./assets/check.svg" alt="it is an icon CHECK" />
+                  рекомендуется специальная обувь
+                </li>
+                <li>
+                  <img class="check" src="./assets/check.svg" alt="it is an icon CHECK" />
+                  автобус до пляжа: бесплатно
+                </li>
+                <li>
+                  <img class="check" src="./assets/check.svg" alt="it is an icon CHECK" />
+                  коралловый (Palma Beach)
+                </li>
+              </ul>
+            </div>
+          </li>
+          <li>
+            Територія
+            <div class="sub-list-wrapper">
+              <ul class="sub__list list">
+                <li>
+                  <img class="check" src="./assets/check.svg" alt="it is an icon CHECK" />
+                  Водні горки: 3
+                </li>
+                <li>
+                  <img class="check" src="./assets/check.svg" alt="it is an icon CHECK" />
+                  дитяча дискотека
+                </li>
+                <li>
+                  <img class="check" src="./assets/check.svg" alt="it is an icon CHECK" />
+                  відділ в дитячому басейні для дорослих (2 без підігріву)
+                </li>
+              </ul>
+            </div>
+          </li>
+          <li>
+            Номер
+            <div class="sub-list-wrapper">
+              <ul class="sub__list list">
+                <li>
+                  <img class="check" src="./assets/check.svg" alt="it is an icon CHECK" />
+                  Водні горки: 3
+                </li>
+                <li>
+                  <img class="check" src="./assets/check.svg" alt="it is an icon CHECK" />
+                  дитяча дискотека
+                </li>
+                <li>
+                  <img class="check" src="./assets/check.svg" alt="it is an icon CHECK" />
+                  відділ в дитячому басейні для дорослих (2 без підігріву)
+                </li>
+              </ul>
+            </div>
+          </li>
+          <li>
+            Розваги
+            <div class="sub-list-wrapper">
+              <ul class="sub__list list">
+                <li>
+                  <img class="check" src="./assets/check.svg" alt="it is an icon CHECK" />
+                  Водні горки: 3
+                </li>
+                <li>
+                  <img class="check" src="./assets/check.svg" alt="it is an icon CHECK" />
+                  дитяча дискотека
+                </li>
+                <li>
+                  <img class="check" src="./assets/check.svg" alt="it is an icon CHECK" />
+                  відділ в дитячому басейні для дорослих (2 без підігріву)
+                </li>
+              </ul>
+            </div>
+          </li>
+          <li>
+            Діти від 8-ми місяців до трьох років
+            <div class="sub-list-wrapper">
+              <ul class="sub__list list">
+                <li>
+                  <img class="check" src="./assets/check.svg" alt="it is an icon CHECK" />
+                  Водні горки: 3
+                </li>
+                <li>
+                  <img class="check" src="./assets/check.svg" alt="it is an icon CHECK" />
+                  дитяча дискотека
+                </li>
+                <li>
+                  <img class="check" src="./assets/check.svg" alt="it is an icon CHECK" />
+                  відділ в дитячому басейні для дорослих (2 без підігріву)
+                </li>
+              </ul>
+            </div>
+          </li>
+          <li>
+            Харчування
+            <div class="sub-list-wrapper">
+              <ul class="sub__list list">
+                <li>
+                  <img class="check" src="./assets/check.svg" alt="it is an icon CHECK" />
+                  Водні горки: 3
+                </li>
+                <li>
+                  <img class="check" src="./assets/check.svg" alt="it is an icon CHECK" />
+                  дитяча дискотека
+                </li>
+                <li>
+                  <img class="check" src="./assets/check.svg" alt="it is an icon CHECK" />
+                  відділ в дитячому басейні для дорослих (2 без підігріву)
+                </li>
+              </ul>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default TourServices;
