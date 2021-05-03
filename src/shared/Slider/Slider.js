d39f34c909a434871d42db6bf95da598c2dd4caa
Sergey <serejabgdn@gmail.com>
2021-05-03 12:43:59 +0300

    changed: slider

--- src/shared/Slider/Slider.js
+++ src/shared/Slider/Slider.js
@@ -1,22 +1,28 @@
-import React from 'react';
-import './Slider.css';
+import React from "react";
+import "./Slider.css";
 
-import {Carousel} from 'react-responsive-carousel';
-import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import { Slide } from "react-slideshow-image";
+import "react-slideshow-image/dist/styles.css";
+const imgs = [
+  { url: "https://img.turne.ua/static/hotels/turciya/kemer/h777/orig/booking777_21.jpg" },
+  { url: "https://img.turne.ua/static/hotels/turciya/kemer/h777/orig/booking777_20.jpg" },
+  { url: "https://img.turne.ua/static/hotels/turciya/kemer/h777/orig/booking777_1.jpg" },
+];
+
+const settings = {
+  easing: "ease",
+  indicators: true,
+};
 
 function Slider() {
   return (
-    <Carousel autoPlay width="100%">
-      <div>
-        <img alt="" src="https://img.turne.ua/static/hotels/turciya/kemer/h777/orig/booking777_21.jpg" />
-      </div>
-      <div>
-        <img alt="" src="https://img.turne.ua/static/hotels/turciya/kemer/h777/orig/booking777_20.jpg" />
-      </div>
-      <div>
-        <img alt="" src="https://img.turne.ua/static/hotels/turciya/kemer/h777/orig/booking777_1.jpg" />
-      </div>
-    </Carousel>
+    <Slide {...settings}>
+      {imgs.map(({ url }) => (
+        <div className="each-slide">
+          <div style={{ backgroundImage: `url(${url})` }}></div>
+        </div>
+      ))}
+    </Slide>
   );
 }
 
