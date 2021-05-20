368764b1eb621809a6e57116053d10f67b9f2367
Sergey <serejabgdn@gmail.com>
2021-05-20 17:12:00 +0300

    replaced: SearchBoard on header

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,17 +8,22 @@ import ToursSearch from '../ToursSearch';
 
 import { Switch, Route, Redirect } from 'react-router-dom';
 import AdminForm from '../admin/AdminForm/AdminForm';
+import Signup from '../auth/Signup';
+import Signin from '../auth/Signin';
+import Header from '../../shared/Header';
 
 function App() {
   return (
     <div className='App'>
       {/* <AdminForm /> */}
-      <SearchBoard />
+      <Header color={true} />
       <Switch>
         <Route path='/tour/:id' component={Tour} />
         <Route path='/search' component={ToursSearch} />
         <Route path='/tour' component={Tour} />
         <Route path='/hot' component={Offer} />
+        <Route path='/register' component={Signup} />
+        <Route path='/in' component={Signin} />
         <Redirect to='/hot' />
       </Switch>
     </div>
