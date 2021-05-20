7fb1dd6a430e1e6ba9d949c55d2539fecc01ee88
Sergey <serejabgdn@gmail.com>
2021-05-20 17:12:40 +0300

    added: Welcome

--- src/components/ToursSearch/ToursSearch.js
+++ src/components/ToursSearch/ToursSearch.js
@@ -3,11 +3,13 @@ import './ToursSearch.css';
 
 import TourCards from './TourCards/TourCards';
 import Filters from './Filters';
+import Welcome from '../../shared/Welcome';
 
 function ToursSearch() {
   return (
-    <div className="wrapper">
-      <section className="ToursSearch">
+    <div className='wrapper'>
+      <Welcome />
+      <section className='ToursSearch'>
         <Filters />
         <TourCards />
       </section>
