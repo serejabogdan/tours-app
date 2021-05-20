0137efe1de4264f5a502fea44c01e8a37f2ed2df
Sergey <serejabgdn@gmail.com>
2021-05-20 18:04:26 +0300

    added: local auth state and handleSubmit

--- src/components/auth/Signin/Signin.js
+++ src/components/auth/Signin/Signin.js
@@ -1,21 +1,43 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Signin.css';
 
+import {signin} from '../../../shared/utils/auth';
+import {useHistory} from 'react-router';
+
 function Signin() {
+  const [formData, setFormData] = useState({login: 'a@gmail.com', password: '123123123'});
+  const history = useHistory();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    signin(formData.login, formData.password);
+    history.push('/hot');
+  }
+
   return (
-    <div className='signin'>
+    <form className='signin' onSubmit={handleSubmit}>
       <label>
         <span>Login:</span>
-        <input className='input' type='text' value={''} onChange={(e) => console.log(e)} />
+        <input
+          className='input'
+          type='text'
+          value={formData.login}
+          onChange={(e) => setFormData((prevState) => ({...prevState, login: e.target.value}))}
+        />
       </label>
       <label>
         <span>Password:</span>
-        <input className='input' type='text' value={''} onChange={(e) => console.log(e)} />
+        <input
+          className='input'
+          type='password'
+          value={formData.password}
+          onChange={(e) => setFormData((prevState) => ({...prevState, password: e.target.value}))}
+        />
       </label>
       <button className='submit-btn btn' type='submit'>
         Go!
       </button>
-    </div>
+    </form>
   );
 }
 
--- src/components/auth/Signup/Signup.js
+++ src/components/auth/Signup/Signup.js
@@ -1,25 +1,52 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Signup.css';
 
+import {signup} from '../../../shared/utils/auth';
+import {useHistory} from 'react-router';
+
 function Signup() {
+  const [formData, setFormData] = useState({login: '', password: '', passwordConfirm: ''});
+  const history = useHistory();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    signup(formData.login, formData.password);
+    history.push('/hot');
+  }
+
   return (
-    <div className='signup'>
+    <form className='signup' onSubmit={handleSubmit}>
       <label>
         <span>Login:</span>
-        <input className='input' type='text' value={''} onChange={(e) => console.log(e)} />
+        <input
+          className='input'
+          type='text'
+          value={formData.login}
+          onChange={(e) => setFormData((prevState) => ({...prevState, login: e.target.value}))}
+        />
       </label>
       <label>
         <span>Password:</span>
-        <input className='input' type='text' value={''} onChange={(e) => console.log(e)} />
+        <input
+          className='input'
+          type='password'
+          value={formData.password}
+          onChange={(e) => setFormData((prevState) => ({...prevState, password: e.target.value}))}
+        />
       </label>
       <label>
-        <span>Conform password:</span>
-        <input className='input' type='text' value={''} onChange={(e) => console.log(e)} />
+        <span>Confirm password:</span>
+        <input
+          className='input'
+          type='password'
+          value={formData.passwordConfirm}
+          onChange={(e) => setFormData((prevState) => ({...prevState, passwordConfirm: e.target.value}))}
+        />
       </label>
       <button className='submit-btn btn' type='submit'>
         Go!
       </button>
-    </div>
+    </form>
   );
 }
 
