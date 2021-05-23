b5b5d7b85ffafa0173cde7750369793c9329412e
Sergey <serejabgdn@gmail.com>
2021-05-23 13:29:17 +0300

    fixed: SET_TOURS state

--- src/redux/reducers/toursReducer/toursReducer.js
+++ src/redux/reducers/toursReducer/toursReducer.js
@@ -1,11 +1,11 @@
-import { SET_TOURS } from '../../types';
+import {SET_TOURS} from '../../types';
 
 const initialState = [];
 
 export const toursReducer = (state = initialState, action) => {
   switch (action.type) {
     case SET_TOURS:
-      return [action.payload];
+      return action.payload;
     default:
       return state;
   }
