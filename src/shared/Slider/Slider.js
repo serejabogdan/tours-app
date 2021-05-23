61d27e29ea6eadaaff1c97d16cff2440c598f506
Sergey <serejabgdn@gmail.com>
2021-05-23 13:28:47 +0300

    added: urls to slider

--- src/shared/Slider/Slider.js
+++ src/shared/Slider/Slider.js
@@ -1,27 +1,24 @@
-import React from "react";
-import "./Slider.css";
+import React from 'react';
+import './Slider.css';
 
-import { Slide } from "react-slideshow-image";
-import "react-slideshow-image/dist/styles.css";
-const imgs = [
-  { url: "https://img.turne.ua/static/hotels/turciya/kemer/h777/orig/booking777_21.jpg" },
-  { url: "https://img.turne.ua/static/hotels/turciya/kemer/h777/orig/booking777_20.jpg" },
-  { url: "https://img.turne.ua/static/hotels/turciya/kemer/h777/orig/booking777_1.jpg" },
-];
+import {Slide} from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css';
 
 const settings = {
-  easing: "ease",
-  indicators: true,
+  easing: 'ease',
+  indicators: true
 };
 
-function Slider() {
+function Slider(props) {
+  const {urls} = props;
   return (
     <Slide {...settings}>
-      {imgs.map(({ url }) => (
-        <div className="each-slide">
-          <div style={{ backgroundImage: `url(${url})` }}></div>
-        </div>
-      ))}
+      {urls &&
+        urls.map((url) => (
+          <div className='each-slide' key={url}>
+            <div style={{backgroundImage: `url(${url})`}}></div>
+          </div>
+        ))}
     </Slide>
   );
 }
