3007424fe47fb974900ca01dae32b7891e27afcc
Sergey <serejabgdn@gmail.com>
2021-05-25 19:48:07 +0300

    added: nav link makeOrder

--- src/shared/Header/Header.js
+++ src/shared/Header/Header.js
@@ -14,6 +14,9 @@ function renderAuthOrLogout(authLinks, userAuth) {
     {
       return (
         <>
+          <NavLink className='header__link' activeClassName='header__link--active' to='/order'>
+            Оформити заявку
+          </NavLink>
           <NavLink className='header__link' activeClassName='header__link--active' to='/profile'>
             Профіль
           </NavLink>
