1a6ba76803520079de21dce5b662f22bdd531bc7
Sergey <serejabgdn@gmail.com>
2021-05-25 14:48:28 +0300

    added: tour nav links

--- src/components/ToursSearch/TourCards/TourCard/TourCard.js
+++ src/components/ToursSearch/TourCards/TourCard/TourCard.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './TourCard.css';
-import hotel from '../../../../assets/hotels/fortuna.jpg';
 import {Link} from 'react-router-dom';
 
 function TourCard(props) {
@@ -10,14 +9,14 @@ function TourCard(props) {
   return (
     <div className='TourCard'>
       <div className='TourCard__img'>
-        <a href='./index.html'>
+        <Link to={`/tour/${id}`} key={id}>
           <img src={urls && urls[0]} alt='It is a hotel' />
-        </a>
+        </Link>
       </div>
       <div className='TourCard__content'>
-        <a href='./index.html' className='TourCard__title'>
+        <Link className='TourCard__title' to={`/tour/${id}`} key={id}>
           {name}, {rate}
-        </a>
+        </Link>
         <div className='TourCard__text TourCard__country'>
           {country}, {resort}
         </div>
@@ -25,7 +24,7 @@ function TourCard(props) {
           З {startDate.toLocaleDateString()} по {endDate.toLocaleDateString()}
         </div>
         <div className='TourCard__text'>Все включено</div>
-        <div className='TourCard__text'>Проезд включен (з м. {city})</div>
+        <div className='TourCard__text'>Переліт (з м. {city})</div>
         <div className='TourCard__text TourCard__description'>{description}</div>
       </div>
       <div className='TourCard__other'>
@@ -35,7 +34,7 @@ function TourCard(props) {
             <span className='price'>{price}</span>
             <span className='currency'> грн</span>
           </div>
-          <Link className='TourCard__btn btn' to={`/tour/${id}`} key={id}>
+          <Link className='TourCard__btn btn' to={`/tour/${id}`}>
             Більше
           </Link>
         </div>
