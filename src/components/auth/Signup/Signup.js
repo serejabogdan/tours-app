5ad436531c58df8ba26c404f53d2627337757478
Sergey <serejabgdn@gmail.com>
2021-05-27 16:46:43 +0300

    added: signup validation

--- src/components/auth/Signup/Signup.js
+++ src/components/auth/Signup/Signup.js
@@ -3,15 +3,28 @@ import './Signup.css';
 
 import {signup} from '../../../shared/utils/auth';
 import {useHistory} from 'react-router';
+import {useForm} from 'react-hook-form';
+import {auth, database} from '../../../firebase.config';
 
 function Signup() {
-  const [formData, setFormData] = useState({login: 'balgar@gmail.com', password: '123123', passwordConfirm: ''});
+  // const [formData, setFormData] = useState({login: 'balgar@gmail.com', password: '123123', passwordConfirm: ''});
+  const {
+    register,
+    handleSubmit,
+    formState: {errors}
+  } = useForm();
+
   const history = useHistory();
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    signup(formData.login, formData.password);
-    history.push('/hot');
+  function onSubmit(data) {
+    const {name, email, tel, password} = data;
+    signup(email, password).then((user) =>
+      auth.onAuthStateChanged((user) => {
+        const ref = database.ref(`users/${user.uid}`);
+        ref.set({name, email, tel});
+        history.push('/search/result');
+      })
+    );
   }
 
   return (
@@ -22,22 +35,38 @@ function Signup() {
         <h1 className='signup__main-title'>Сьогодні.</h1>
       </div>
       <div className='signup__right-side'>
-        <form className='signup__form' onSubmit={handleSubmit}>
+        <form className='signup__form' onSubmit={handleSubmit(onSubmit)}>
           <label>
             <span>І'мя:</span>
-            <input className='input' type='text' />
+            <input
+              className={`input ${errors.name && 'input-error'}`}
+              type='text'
+              {...register('name', {required: true, pattern: /^\D+$/g})}
+            />
           </label>
           <label>
             <span>E-mail:</span>
-            <input className='input' type='email' />
+            <input
+              className={`input ${errors.email && 'input-error'}`}
+              type='email'
+              {...register('email', {required: true, pattern: /.+@.+\..+/i})}
+            />
+          </label>
+          <label>
+            <span>Номер:</span>
+            <input
+              className={`input ${errors.tel && 'input-error'}`}
+              type='tel'
+              {...register('tel', {required: true, pattern: /^0\d{8}\d$/i})}
+            />
           </label>
           <label>
             <span>Пароль:</span>
-            <input className='input' type='password' />
-          </label>
-          <label>
-            <span>Пароль:</span>
-            <input className='input' type='password' />
+            <input
+              className={`input ${errors.password && 'input-error'}`}
+              type='password'
+              {...register('password', {required: true})}
+            />
           </label>
           <button className='submit-btn btn' type='submit'>
             До зірок!
