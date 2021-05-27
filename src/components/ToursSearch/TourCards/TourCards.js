287012d5cc1ab344b39beb33cd111e5fa5872abe
Sergey <serejabgdn@gmail.com>
2021-05-27 12:06:33 +0300

    fixed: added tour to props

--- src/components/ToursSearch/TourCards/TourCards.js
+++ src/components/ToursSearch/TourCards/TourCards.js
@@ -32,7 +32,7 @@ function TourCards({tours, search, selected, minPrice, maxPrice, tourName, setTo
   return (
     <div className='TourCards'>
       {resultToursAfterFilters.length
-        ? resultToursAfterFilters.map((tour) => <TourCard key={tour.id} search={search} />)
+        ? resultToursAfterFilters.map((tour) => <TourCard key={tour.id} tour={tour} search={search} />)
         : 'Немає турів, котрі б задовольняли умову вибірки'}
     </div>
   );
